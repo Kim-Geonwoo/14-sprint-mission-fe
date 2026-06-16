abf1cf3717a6a7221a4340b28b5645ea6bd52072
Geonwoo_Kim <59507879+Kim-Geonwoo@users.noreply.github.com>
2026-06-16 18:32:46 +0900

    프로젝트 시작: js기반 react(with vite) 프로젝트 생성 및 tailwindCSS 연결
    - Javascript 기반 React 프로젝트를 Vite를 사용하여 생성하였습니다.
    - 프로젝트에 TailwindCSS를 추가하였습니다.
    - 기존의 샘플 프로젝트에서 불필요한 코드들을 정리 하였습니다.

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section class="mt-8 flex flex-col items-center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="tooling" alt="Vite logo" />
+        </div>
+        <div class="text-center mt-6">
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
