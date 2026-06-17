26bd78f1e601fdb9bdd5bac794eb8d45ce47b331
Geonwoo_Kim <59507879+Kim-Geonwoo@users.noreply.github.com>
2026-06-17 16:06:55 +0900

    리소스 추가 및 Header 작업: 폰트 및 브랜드아이콘 svg 추가, Header 컴포넌트 작업
     - Pretendard 폰트 추가 및 Rokaf Sans(바른공군체) 리소스 추가
     - 판다마켓 브랜드 아이콘(솔리드) 파일 추가
     - Header 컴포넌트 구현(완료)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,25 @@
 import ButtonXS from './ButtonXS';
-import Pagination from './Pagination';
-
+import BrandIcon from '../assets/brand_icon-solid.svg';
 
 const Header = () => {
   return (
-    <header>
-      <div className="flex justify-content space-between align-items-center bg-white border-b-[1px] border-b-gray-200 px-[200px] py-0">
-        <div>
-          <ButtonXS state="default" text="로그인" />
-        </div>
+    <div class="w-full px-48 bg-white border-b border-neutral-200 inline-flex justify-between items-center">
+      <div class="flex justify-start items-center gap-x-[24px]">
+          <div class="inline-flex shrink-0 justify-start items-center gap-2">
+            <img src={BrandIcon} alt="Brand Icon" />
+            <div class="justify-center text-blue-500 text-2xl font-bold font-['ROKAF_Sans']">판다마켓</div>
+          </div>
+          <div class="size- flex justify-center items-center">
+              <div class="size- px-3.5 py-5 flex justify-center items-center gap-2.5 overflow-hidden">
+                  <div class="text-center justify-center text-gray-600 text-lg font-bold font-['Pretendard'] leading-6">자유게시판</div>
+              </div>
+              <div class="size- px-3.5 py-5 flex justify-center items-center gap-2.5 overflow-hidden">
+                  <div class="text-center justify-center text-gray-600 text-lg font-bold font-['Pretendard'] leading-6">중고마켓</div>
+              </div>
+          </div>
       </div>
-      <h1>My App</h1>
-      <Pagination />
-
-    </header>
+      <ButtonXS state="default" text="로그인" />
+    </div>
   );
 };
 
